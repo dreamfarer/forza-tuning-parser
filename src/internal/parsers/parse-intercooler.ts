52033915159419d1d6f83cd516d02321918bd6a4
dreamfarer <dreamfarer@pm.me>
2026-06-12 16:57:20 +0200

    chore: refactor parseIntercooler to output UpgradeField

--- src/internal/parsers/parse-intercooler.ts
+++ src/internal/parsers/parse-intercooler.ts
@@ -1,18 +1,20 @@
 import type { UpgradeField } from '../../types';
 
-const options = ['Stock', 'Street', 'Sport', 'Race'];
+const options = ['No Intercooler', 'Stock', 'Street', 'Sport', 'Race'];
 
 /**
  * Get the upgrade name from the raw upgrade ID.
  */
 function getUpgrade(raw: number): string | null {
   switch (raw % 1000) {
+    case -1:
+      return 'No Intercooler';
     case 0:
-      return 'Stock';
+      return 'Stock (Street if No Intercooler possible)';
     case 1:
-      return 'Street';
+      return 'Street (Sport if No Intercooler possible)';
     case 2:
-      return 'Sport';
+      return 'Sport (Race if No Intercooler possible)';
     case 3:
       return 'Race';
     default:
@@ -21,16 +23,16 @@ function getUpgrade(raw: number): string | null {
 }
 
 /**
- * Parse an arbitrary upgrade grade from a binary Forza tuning file.
+ * Parse the intercooler upgrade from a binary Forza tuning file.
  *
  * @param view The DataView to read from.
  * @param byteOffset The byte offset to read from.
  *
- * @returns The parsed upgrade grade.
+ * @returns The parsed intercooler upgrade.
  *
  * @throws RangeError If the upgrade ID is invalid.
  */
-export function parseDefault(
+export function parseIntercooler(
   view: DataView<ArrayBufferLike>,
   byteOffset: number,
 ): UpgradeField {
